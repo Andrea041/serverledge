e3f0f747701593c0df6033e470c615421df04d82
andreaandreoli <aandreo.2001@gmail.com>
2025-10-18 12:51:10 +0200

    change mem config

--- scripts/create-functions.sh
+++ scripts/create-functions.sh
@@ -5,7 +5,7 @@
 # bin/serverledge-cli create -f imageclass --runtime custom --custom_image grussorusso/serverledge-imageclass --memory 1024 -H "$1"
 
 ../bin/serverledge-cli create -H 192.168.122.13 -P 1323 -f f1 --memory 512 --src ../examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
-../bin/serverledge-cli create -H 192.168.122.13 -P 1323 -f f2 --memory 512 --src ../examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
+../bin/serverledge-cli create -H 192.168.122.13 -P 1323 -f f2 --memory 256 --src ../examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
 ../bin/serverledge-cli create -H 192.168.122.13 -P 1323 -f f3 --memory 128 --src ../examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
 ../bin/serverledge-cli create -H 192.168.122.13 -P 1323 -f f4 --memory 1024 --src ../examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
-../bin/serverledge-cli create -H 192.168.122.13 -P 1323 -f f5 --memory 256 --src ../examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
+../bin/serverledge-cli create -H 192.168.122.13 -P 1323 -f f5 --memory 512 --src ../examples/fibonacciNout.py --runtime python310 --handler "fibonacciNout.handler"
